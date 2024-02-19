c5e2ff34e88150351304c6617a392e7847444283
Aurélien <aurelien.strimelle@bstorm.be>
2024-02-19 15:06:14 +0100

    Gestion du formulaire de contact

--- routes/home.router.js
+++ routes/home.router.js
@@ -6,5 +6,6 @@ const homeRouter = require('express').Router();
 homeRouter.get('/', homeController.index);
 homeRouter.get('/contact', homeController.contact);
 homeRouter.post('/contact', homeController.contact_POST);
+homeRouter.get('/contact/confirm', homeController.confirm);
 
 module.exports = homeRouter;
